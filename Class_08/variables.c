b2a39c7d7bdafbbd3d04b935f1b8d04b14ed99d7
Robcons123 <Roberts.Cipruss@edu.rtu.lv>
2023-11-26 23:18:49 +0200

    Class_PU1

---
+++ Class_08/variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+int i; //diskrētā mainīgā 
+
+printf("i mainīgā izmērs baitos: %ld\n", sizeof(i));
+printf("i mainīgā atrašanās vieta atmiņā (adrese jeb norāde): %p\n",&i);
+printf("i mainīgā vērtība uzreiz pēc deklarēšanas (simbols): %c\n",i);
+printf("i mainīgā vērtība uzreiz pēc deklarēšanas (dec): %d\n",i);
+printf("i mainīgā vērtība uzreiz pēc deklarēšanas (oct): %#o\n",i);
+printf("i mainīgā vērtība uzreiz pēc deklarēšanas (hex): %#x\n",i);
+printf("i mainīgā vērtība uzreiz pēc deklarēšanas (real): %c\n",i);
+printf("i mainīgā vērtība uzreiz pēc deklarēšanas (real): %c\n",i);
+
+
+}
---
+++ PU1/PU1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+long int a,b,c;
+printf("Ievadiet pirmo skaitli: ");
+scanf("%ld",&a);
+printf("Ievadiet otro skaitli: ");
+scanf("%ld",&b);
+c=a*b;
+printf("Skaitlu %ld un %ld reizinajums ire %ld \n",a,b,c);
+
+return 0;
+}
